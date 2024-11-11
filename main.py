50b2245d79009cf04b0bbb9f5af693f24661d56e
PedroSmaxY <pedrohnovais@outlook.com>
2024-11-11 18:24:10 -0300

    refactor(cors): cors control added

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from app.api.routes import routes
 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="profile-insight-api",
     version="1.0.0",
     docs_url="/api/docs",
     redoc_url="/api/redoc"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
